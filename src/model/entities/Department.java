afe41fd566c484f471c21109f57a19be1c55648b
Geovane Junior <geovane.gjunior@gmail.com>
2019-03-17 10:38:10 -0300

    Adicionando acesso ao Banco de Dados

--- src/model/entities/Department.java
+++ src/model/entities/Department.java
@@ -10,7 +10,6 @@ public class Department implements Serializable {
 	private String name;
 	
 	public Department() {
-		
 	}
 
 	public Department(Integer id, String name) {
@@ -35,16 +34,10 @@ public class Department implements Serializable {
 	}
 
 	@Override
-	public String toString() {
-		return "Department [id=" + id + ", name=" + name + "]";
-	}
-
-	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		result = prime * result + ((name == null) ? 0 : name.hashCode());
 		return result;
 	}
 
@@ -62,12 +55,11 @@ public class Department implements Serializable {
 				return false;
 		} else if (!id.equals(other.id))
 			return false;
-		if (name == null) {
-			if (other.name != null)
-				return false;
-		} else if (!name.equals(other.name))
-			return false;
 		return true;
 	}
-	
+
+	@Override
+	public String toString() {
+		return "Department [id=" + id + ", name=" + name + "]";
+	}
 }
